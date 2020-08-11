ea1590d94535ac1e98df4a63594ecc80699a1fc1
quantumsheep <quantumsheep@users.noreply.github.com>
2020-08-11 02:28:17 +0200

    Use tmpnam_s for temporary filename on Windows

--- src/Helpers.cpp
+++ src/Helpers.cpp
@@ -30,7 +30,10 @@ bool Helpers::starts_with(const std::string &str, const std::string &starting)
 std::string Helpers::temporary_filename()
 {
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
-    return std::tmpnam(nullptr);
+    char filename[L_tmpnam_s];
+    tmpnam_s(filename, L_tmpnam_s);
+
+    return filename;
 #else
     auto filename = fs::temp_directory_path().u8string() + "XXXXXXXXXXXXXXXX";
     mkstemp(filename.data());
